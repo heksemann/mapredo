d953dc6b68eee0a05e380b0ef0f157ab6c38ed64
Kjell Irgens <hextremist@gmail.com>
2015-01-05 23:36:01 +0100

    New output_buffer function.
    This may be used when outputting data to the collector, so avoid allocating
    new string objects.

--- mapredo/mapreducer.h
+++ mapredo/mapreducer.h
@@ -22,17 +22,11 @@ namespace mapredo
 			   || std::is_same<T,int64_t>::value
 			   || std::is_same<T,double>::value,
 			   "Key needs to be char*, int64_t or double");
-	    if (std::is_same<T,char*>::value)
-	    {
-		set_type (keytype::STRING);
-	    }
-	    else if (std::is_same<T,int64_t>::value)
-	    {
-		set_type (keytype::INT64);
-	    }
+	    if (std::is_same<T,char*>::value) set_type (keytype::STRING);
+	    else if (std::is_same<T,int64_t>::value) set_type (keytype::INT64);
 	    else set_type (keytype::DOUBLE);
 	}
-	virtual ~mapreducer() {}
+	virtual ~mapreducer() {if (_buffer) delete[] _buffer;}
 
 	/**
 	 * Reduce function.
@@ -45,9 +39,58 @@ namespace mapredo
 
 	bool reverse() const {return _reverse;}
 	bool numeric() const {return _numeric;}
+
+    protected:
+	/**
+	 * Get a pointer to a buffer that may be used for output from
+	 * both the map() or reduce() functions.  If the buffer is
+	 * used, this function should be called every time; a
+	 * different buffer may be returned as the size is increased.
+	 * @param size the minimum size of the allocated buffer
+	 * @param realloc if true, copy over the data from the previous
+	 *                buffer when a new allocation is needed.
+	 */
+	char* output_buffer (const size_t size, const bool realloc = false) {
+	    if (size > _buffer_size)
+	    {
+		if (realloc)
+		{
+		    if (size < _prev_size)
+		    {
+			throw std::runtime_error
+			    ("output_buffer() can not be used  with decreased"
+			     " buffer size and realloc flag");
+		    }
+
+		    if (!_buffer) _buffer_size = 128;
+		    while (size > _buffer_size) _buffer_size *= 2;
+		    if (_prev_size > 0)
+		    {
+			char* oldbuffer = _buffer;
+			_buffer = new char[_buffer_size];
+			memcpy (_buffer, oldbuffer, _prev_size);
+			delete oldbuffer;
+		    }
+		    else _buffer = new char[_buffer_size];
+		    _prev_size = size;
+		}
+		else
+		{
+		    if (_buffer) delete[] _buffer;
+		    else _buffer_size = 128;
+		    while (size > _buffer_size) _buffer_size *= 2;
+		    _buffer = new char[_buffer_size];
+		}
+	    }
+	    return _buffer;
+	}
+
     private:
 	const bool _reverse;
 	const bool _numeric;
+	char* _buffer = nullptr;
+	size_t _buffer_size = 0;
+	size_t _prev_size = 0;
     };
 }
 
