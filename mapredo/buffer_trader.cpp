d8792eb496ecc6194b803957bf43a4011ea99200
Kjell Irgens <hextremist@gmail.com>
2015-02-01 15:00:05 +0100

    Producer/consumer engine now works it seems.

--- mapredo/buffer_trader.cpp
+++ mapredo/buffer_trader.cpp
@@ -1,18 +1,21 @@
 
+#include <iostream>
 #include "buffer_trader.h"
 
 buffer_trader::buffer_trader (const size_t buffer_size,
 			      const size_t num_buffers,
-			      const size_t num_threads) :
-    _buffer_size (buffer_size), _max_buffers (num_buffers)
+			      const size_t num_consumers) :
+    _buffer_size (buffer_size),
+    _max_buffers (num_buffers),
+    _num_consumers (num_consumers)
 {
-    if (num_buffers < num_threads + 1)
+    if (num_buffers < num_consumers + 1)
     {
-	throw std::runtime_error ("Too few buffers");
+	throw std::runtime_error (std::string("Too few buffers in ")
+				  + __FUNCTION__);
     }
 }
 
-#include <iostream>
 input_buffer*
 buffer_trader::producer_get()
 {
@@ -28,13 +31,14 @@ buffer_trader::producer_get()
 	(std::string(__FUNCTION__) + " called too many times");
 }
 
-#include <iostream>
 input_buffer*
 buffer_trader::producer_swap (input_buffer* buffer)
 {
     std::unique_lock<std::mutex> lock(_mutex);
+    bool was_empty = _filled_buffers.empty();
 
     _filled_buffers.push (buffer);
+    if (was_empty) _consumer_cond.notify_one();
     
     if (_all_buffers.size() < _max_buffers)
     {
@@ -42,7 +46,7 @@ buffer_trader::producer_swap (input_buffer* buffer)
 	return &_all_buffers.back();
     }
 
-    while (_empty_buffers.empty()) _cond.wait (lock);
+    while (_empty_buffers.empty()) _producer_cond.wait (lock);
 
     buffer = _empty_buffers.top();
     _empty_buffers.pop();
@@ -55,12 +59,22 @@ buffer_trader::consumer_get()
 {
     std::unique_lock<std::mutex> lock(_mutex);
 
-    while (_filled_buffers.empty()) _cond.wait (lock);
+    while (!_waiting_final && _filled_buffers.empty())
+    {
+	_consumer_cond.wait (lock);
+    }
 
-    auto* buffer = _filled_buffers.top();
-    _filled_buffers.pop();
+    if (_filled_buffers.size())
+    {
+	auto* buffer = _filled_buffers.top();
+	_filled_buffers.pop();
 
-    return buffer;
+	return buffer;
+    }
+
+    std::cerr << "Finished0\n";
+    consumer_finish();
+    return nullptr;
 }
 
 input_buffer*
@@ -68,12 +82,71 @@ buffer_trader::consumer_swap (input_buffer* buffer)
 {
     std::unique_lock<std::mutex> lock(_mutex);
 
-    _empty_buffers.push (buffer);
+    if (_waiting_final)
+    {
+	if (_filled_buffers.empty())
+	{
+	    std::cerr << "Finished1\n";
+	    consumer_finish();
+	    return nullptr;
+	}
+	else
+	{
+	    std::cerr << "Almost finished\n";
+	    buffer = _filled_buffers.top();
+	    _filled_buffers.pop();
+
+	    return buffer;
+	}
+    }
     
-    while (_filled_buffers.empty()) _cond.wait (lock);
+    bool was_empty = _empty_buffers.empty();
 
-    buffer = _filled_buffers.top();
-    _filled_buffers.pop();
+    _empty_buffers.push (buffer);
+    if (was_empty) _producer_cond.notify_one();
+    
+    while (!_waiting_final && _filled_buffers.empty())
+    {
+	_consumer_cond.wait (lock);
+    }
+    if (_filled_buffers.size())
+    {
+	buffer = _filled_buffers.top();
+	_filled_buffers.pop();
 
-    return buffer;
+	return buffer;
+    }    
+
+    std::cerr << "Finished3\n";
+    consumer_finish();
+    return nullptr;
+}
+
+void
+buffer_trader::wait_emptied()
+{
+    std::unique_lock<std::mutex> lock(_mutex);
+    std::cerr << "Final wait " << _filled_buffers.size() << "\n";
+
+    _waiting_final = _num_consumers;
+    _consumer_cond.notify_all();
+
+    do _producer_cond.wait (lock);
+    while (_waiting_final != -1);
+}
+
+void
+buffer_trader::consumer_finish()
+{
+    if (_waiting_final < 0)
+    {
+	throw std::runtime_error (std::string("Incorrect use of ")
+				  + __FUNCTION__);
+    }
+    _waiting_final--;
+    if (_waiting_final == 0)
+    {
+	_waiting_final = -1;
+	_producer_cond.notify_one();
+    }
 }
