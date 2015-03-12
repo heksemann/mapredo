fd08001126d0465f48e9e3d3c0bc707d1aa118f6
Kjell Irgens <kjirgens@localhost.localdomain>
2015-03-12 11:05:53 -0400

    Minor cleanups.

--- plugins/wordsort.h
+++ plugins/wordsort.h
@@ -8,15 +8,11 @@
 /**
  * Class used to sort words on popularity.
  */
-class wordsort : public mapredo::mapreducer<int64_t>
+class wordsort final : public mapredo::mapreducer<int64_t>
 {
 public:
-    wordsort() {}
-    virtual ~wordsort() {}
     void map (char* line, const int length, mapredo::collector& output);
     void reduce (int64_t key, vlist& values, mapredo::collector& output);
-
-private:    
 };
 
 #endif
