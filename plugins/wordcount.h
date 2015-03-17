286248110b0bf512f8b6d1189ea31bea6aad768c
Kjell Irgens <hextremist@gmail.com>
2015-03-17 22:25:22 +0100

    Removed unnecessary boiler plate.

--- plugins/wordcount.h
+++ plugins/wordcount.h
@@ -8,16 +8,12 @@
 /**
  * Class used to count and sort words on popularity.
  */
-class wordcount : public mapredo::mapreducer<char*>
+class wordcount final : public mapredo::mapreducer<char*>
 {
 public:
-    wordcount() {}
-    virtual ~wordcount() {}
     void map (char* line, const int length, mapredo::mcollector& output);
     void reduce (char* key, vlist& values, mapredo::rcollector& output);
     bool reducer_can_combine() const {return true;}
-
-private:    
 };
 
 #endif
